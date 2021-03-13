18a0c6291cce53d36457bc9e77757538380fdf0d
bhoirvarun6@gmail.com <bhoirvarun6@gmail.com>
2021-03-13 13:39:28 +0530

    timer publisher

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -8,108 +8,26 @@
 import CoreHaptics
 import SwiftUI
 
-// vibrations
-struct ContentView3: View {
-    @State private var engine: CHHapticEngine?
+
+struct ContentView: View {
+    let timer = Timer.publish(every: 1, tolerance: 0.5, on: .main, in: .common).autoconnect()
+    @State private var count = 0
     var body: some View {
-        Text("")
-            .onAppear {
-                makeEngine()
-            }.onTapGesture {
-                complexSuccess()
-            }
-    }
-    
-    func simpleSuccess() {
-        let generator = UINotificationFeedbackGenerator()
-        generator.notificationOccurred(.success)
-    }
-    
-    func makeEngine() {
-        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
-        do {
-            engine = try CHHapticEngine()
-        } catch {
-            print("Error while creating engine - \(error.localizedDescription)")
-        }
-    }
-    
-    func complexSuccess() {
-        var events = [CHHapticEvent]()
-        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
-        let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(1))
-        let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(1))
-        let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 0)
-        events.append(event)
-        
-        // crate pattern of those events and play immediately
-        do {
-            let pattern = try CHHapticPattern(events: events, parameters: [])
-            let player = try engine?.makePlayer(with: pattern)
-            try player?.start(atTime: 0)
-        } catch {
-            print("Failed to play pattern - \(error.localizedDescription)")
-        }
+        Text("Varun")
+            .onReceive(timer, perform: { time in
+                if count == 5 {
+                    timer.upstream.connect().cancel()
+                } else {
+                    print("time is now \(time)")
+                }
+                count += 1
+            })
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView3()
+        ContentView()
     }
 }
 
-
-
-// gestures
-struct ContentView2: View {
-    @State private var offset: CGSize = .zero
-    @State private var isDragging = false
-    var body: some View {
-        let dragGesture = DragGesture().onChanged { (value) in
-            
-                offset = value.translation
-            
-        }
-        .onEnded { (value) in
-            isDragging = false
-            offset = .zero
-        }
-        
-        let longPressGesture = LongPressGesture(minimumDuration: 1, maximumDistance: 100).onChanged({ (value) in
-            
-        }).onEnded { (value) in
-            
-                isDragging = true
-            
-            
-        }
-        let combineGesture = longPressGesture.sequenced(before: dragGesture)
-        Circle()
-            .fill(Color.red)
-            .frame(width: 50, height: 50)
-            .scaleEffect(isDragging ? 1.5 : 1)
-            .offset(offset)
-            .gesture(combineGesture)
-            .animation(.default)
-        
-
-    }
-}
-
-// hit testing
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Text("Hello")
-            Spacer()
-                .frame(width: 100, height: 100)
-            Text("World")
-        }
-        //.contentShape(Rectangle())
-        .allowsHitTesting(false)
-        .onTapGesture {
-            print("VStack tapped")
-        }
-    }
-}
